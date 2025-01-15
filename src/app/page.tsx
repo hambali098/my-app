0683cd7fa676e53486318161beb228e8833960df
hambali098 <hambalitubagus@gmail.com>
2025-01-15 23:10:54 +0700

    test deployement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">bacot lah lu wnadklnawkldn alkswdlkamnwd</h1>
+      <h1 className="text-4xl font-bold">bacot lah asdasd lu hahaha wnadklnawkldn alkswdlkamnwd</h1>
     </main>
   )
 }
