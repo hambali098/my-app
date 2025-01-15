5b071d15bf5f8d21a0b80a260f93284ee8ba1aad
hambali098 <hambalitubagus@gmail.com>
2025-01-15 23:14:53 +0700

    adasdas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">awal tai kuda</h1>
+      <h1 className="text-4xl font-bold">awal ganteng</h1>
     </main>
   )
 }
